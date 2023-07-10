d6486d5e2086fb4d6fd0843a19e6ea9a6817b3c0
Azzam Azzam <nive54@gmail.com>
2023-07-10 23:07:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function change(photo) {
+  document.getElementById("mainphoto").src = photo.src;
+}
+function colorChange(color) {
+  document.querySelector(".container").style.backgroundColor = color;
+}
+
+// function style0() {
+//   document.getElementById("mainphoto").src = "/imgs/0.png";
+//   document.querySelector(".container").style.backgroundColor = "black";
+// }
+// function style1() {
+//   document.getElementById("mainphoto").src = "/imgs/1.png";
+//   document.querySelector(".container").style.backgroundColor = "#247ce8";
+// }
+// function style2() {
+//   document.getElementById("mainphoto").src = "/imgs/2.png";
+//   document.querySelector(".container").style.backgroundColor = "#1e1e1e";
+// }
+// function style3() {
+//   document.getElementById("mainphoto").src = "/imgs/3.png";
+//   document.querySelector(".container").style.backgroundColor = "#c79b53";
+// }
+// function style4() {
+//   document.getElementById("mainphoto").src = "/imgs/4.png";
+//   document.querySelector(".container").style.backgroundColor = "#c82525";
+// }
